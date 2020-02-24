9eb907fb442456afe8d1ec1614cd35846dfa9816
Tech Backpack <techbackpackonline@gmail.com>
2020-02-24 07:42:24 +0200

    Exploring Android Studio

---
+++ ExploringAndroidStudio/app/src/main/java/com/example/exploringandroidstudio/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.exploringandroidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        var counter = 0
+
+        button.setOnClickListener(View.OnClickListener {
+            textView.text = "You've clicked the button ${++counter} times!"
+            if (counter >= 10) {
+                counter = 0
+            }
+        })
+
+    }
+}
